f544dbffff5cfb369b2c2c157576cb10bda55142
Ghinafairuznazilah <ghina.fairuz.nazilah@gmail.com>
2021-01-12 15:35:34 +0700

    commit kedua
    authorized by : @Ghinafairuznazilah

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:futurehouse/buttomnav.dart';
 import 'package:futurehouse/splashscreen.dart';
 import 'package:futurehouse/loginscreen.dart';
 import 'package:futurehouse/homescreen.dart';
+import 'package:futurehouse/registerscreen.dart';
 
 
-void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
 
@@ -21,7 +29,8 @@ class MyApp extends StatelessWidget {
         SplashScreen.id: (context) => SplashScreen(),
         LoginScreen.id: (context) => LoginScreen(),
         HomeScreen.id: (context) => HomeScreen(),
-      
+        RegisterScreen.id: (context) => RegisterScreen(),
+        Buttomnav.id: (context) => Buttomnav(),
       },
     );
   }
